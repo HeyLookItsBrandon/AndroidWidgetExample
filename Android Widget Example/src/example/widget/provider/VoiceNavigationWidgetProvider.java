143c564e40f743cbf15b08e8662d80359c6ab080
Brandon Rich <dev@coalminesoftware.com>
2014-12-02 22:53:31 -0700

    Got simple appwidget working.

---
+++ Android Widget Example/src/example/widget/provider/VoiceNavigationWidgetProvider.java
@@ -0,0 +1,7 @@
+package example.widget.provider;
+
+import android.appwidget.AppWidgetProvider;
+
+public class VoiceNavigationWidgetProvider extends AppWidgetProvider {
+	// Override what you need here.
+}
