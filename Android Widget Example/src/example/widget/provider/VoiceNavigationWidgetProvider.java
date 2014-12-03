4e2fc487368f52e4ca1440e6405bfdf8479815be
Brandon Rich <dev@coalminesoftware.com>
2014-12-02 23:02:41 -0700

    Updated widget to launch the main Activity on click.

--- Android Widget Example/src/example/widget/provider/VoiceNavigationWidgetProvider.java
+++ Android Widget Example/src/example/widget/provider/VoiceNavigationWidgetProvider.java
@@ -1,7 +1,31 @@
 package example.widget.provider;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import example.widget.MainActivity;
+import example.widget.R;
 
 public class VoiceNavigationWidgetProvider extends AppWidgetProvider {
-	// Override what you need here.
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for(int appWidgetId : appWidgetIds) {
+
+            // Create an Intent to launch the desired Activity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the widget and attach a click listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.voice_navigation_widget); // TODO Which layout should this used, based on the main vs. lockscreen widget
+            views.setOnClickPendingIntent(R.id.successLabel, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+	}
 }
